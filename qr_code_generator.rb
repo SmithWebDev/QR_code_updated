a6eeee2b8bd4f43b66b655cd6f4e8c681e20f149
Darrick Smith <smithwebdd@gmail.com>
2023-01-20 23:25:04 -0500

    initial attempt to refactor

--- qr_code_generator.rb
+++ qr_code_generator.rb
@@ -1,27 +1,50 @@
 require 'rqrcode'
 
-puts "===================================================================================================="
-puts "==========                               QR Code Generator.                                ========="
-puts "===================================================================================================="
-sleep 1
-puts
-puts
+def spacing
+  puts
+  sleep 1
+end
+
+def color(color:)
+  color == '' ? ('black') : (color)
+end
+
+def fill(fill:)
+  fill == '' ? ('white') : (fill)
+end
+
+def size(size:)
+  size == 0 ? (300) : (size)
+end
+
+#def output(color: 'black', fill: 'white', size: 300)
+#  puts fill
+#  puts color
+#  puts size
+#end
+
+puts "================================================================================"
+puts "==========                     QR Code Generator.                      ========="
+puts "================================================================================"
+
+spacing
+
 puts "-- Please enter the website that you would like to make into a QR Code --"
 puts '--------------------------------------------------------------------------------'
 url = gets.chomp
-puts "Thank you."
-puts
-sleep 1
+
+spacing
 
 puts "-- What would you like to name the QR Code file? --"
 puts '--------------------------------------------------------------------------------'
 filename = gets.chomp
-puts
-sleep 1
+
+spacing
 
 puts '-- Did you want to use specific (hex) colors? (Y/N) --'
 puts '--------------------------------------------------------------------------------'
 decision = gets.downcase.chomp
+
 case decision
 when 'y'
   puts '-- What is the primary color for the QR Code? --'
@@ -35,17 +58,17 @@ when 'y'
   puts '--------------------------------------------------------------------------------'
   print 'Background Color: #'
   fill = gets.chomp
-  puts
-  sleep 1
 else
   color = 'black'
   fill = 'white'
 end
 
+spacing
+
 puts '-- What size would you like the QR Code to be? (whole number) --'
 puts '--------------------------------------------------------------------------------'
 size = gets.chomp.to_i
-puts
+spacing
 
 puts 'One moment while we generate the QR Code'
 puts
@@ -78,12 +101,9 @@ puts 'Your QR Code has been generated and the image can be found in the same dir
 puts
 puts 'Thank you for using the QR Code Generator'
 
-#
-#fill == '' ? ('white') : (fill)
-#color == '' ? ('black') : (color)
-#size == 0 ? (300) : (size)
+output
 
-puts "The color of the QR Code information will be #{color == '' ? ('black') : (color)}. The background color will be #{fill == '' ? ('white') : (fill)}. Lastly, the desired size for the QR Code image is #{size == 0 ? (300) : (size)}px"
+#puts "The color of the QR Code information will be #{color == '' ? ('black') : (color)}. The background color will be #{fill == '' ? ('white') : (fill)}. Lastly, the desired size for the QR Code image is #{size == 0 ? (300) : (size)}px"
 
 #qrcode = RQRCode::QRCode.new("#{url}")
 #
